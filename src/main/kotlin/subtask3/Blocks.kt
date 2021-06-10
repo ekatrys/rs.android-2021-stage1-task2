666f8d999459549584f5fa49b2cc4b8014750e50
Ekaterina <ekatrys@gmail.com>
2021-06-10 18:36:46 +0200

    check 2

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -8,13 +8,12 @@ class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-//        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-//        return when (blockB) {
-//            String::class -> blockA.filterIsInstance<String>().joinToString(separator = "")
-//            Int::class -> blockA.filterIsInstance<Int>().sum()
-//            LocalDate::class -> blockA.filterIsInstance<LocalDate>().sorted().last().format(formatter)
-//            else -> "фигня какая-то а не блок"
-//        }
-        return "jk"
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        return when (blockB) {
+            String::class -> blockA.filterIsInstance<String>().joinToString(separator = "")
+            Int::class -> blockA.filterIsInstance<Int>().sum()
+            LocalDate::class -> blockA.filterIsInstance<LocalDate>().sorted().last().format(formatter)
+            else -> "фигня какая-то а не блок"
+        }
     }
 }
